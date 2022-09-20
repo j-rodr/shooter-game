2992312bb659b732f673acfe01c37faa93001367
José Rodríguez <rodriguezbidojosegabriel@gmail.com>
2022-09-19 21:47:44 -1100

    game logic and assets

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 let canvas = document.getElementById("canvas")
 let ctx = canvas.getContext("2d")
 let runGame = false
+let lostGame = false
+let clearScreen = false
+let timerInterval
 
 const scoreElement = document.getElementById("score")
 const livesElement = document.getElementById("lives")
@@ -10,6 +13,21 @@ const lostMessageElement = document.getElementById("game-over")
 const startBtn = document.getElementById("start-btn")
 const scoreRecordElement = document.getElementById("score-record")
 const starsRecordElement = document.getElementById("stars-record")
+const minutes = document.getElementById("minutes")
+const seconds = document.getElementById("seconds")
+
+if (!localStorage.getItem("score-record") && !localStorage.getItem("stars-record")) {
+
+   localStorage.setItem("score-record", 0)
+   localStorage.setItem("stars-record", 0)
+
+}
+
+scoreRecordElement.innerText = localStorage.getItem("score-record")
+starsRecordElement.innerText = localStorage.getItem("stars-record")
+localStorage.setItem("score", 0)
+localStorage.setItem("lives", 3)
+localStorage.setItem("stars", 0)
 
 /* 1. Responsive canvas */
 
@@ -312,4 +330,298 @@ class Heart extends GameImage {
       }
 
    }
+}
+
+/* 3. Game logic */
+
+startBtn.addEventListener("click", function () {
+
+   runGame = true
+
+   this.style.display = "none"
+
+   timerInterval = setInterval(() => {
+
+      if (+seconds.innerText > 0 && +minutes.innerText >= 0) {
+
+         seconds.innerText = `${String(+seconds.innerText - 1).padStart(2, "0")}`
+
+         if (+seconds.innerText === 0 && +minutes.innerText === 0) {
+            lostGame = true
+            document.getElementById("lost-title").innerHTML = "Time's up!<br>Reload to play again"
+            return;
+         }
+      }
+
+      else {
+
+         if (+minutes.innerText > 0) minutes.innerText = `${String(+minutes.innerText - 1).padStart(2, "0")}`
+
+         seconds.innerText = "59"
+
+      }
+
+   }, 1000)
+
+   gameLoop()
+
+})
+
+// Scorekeeping
+function updateScore(points) {
+
+   let score = parseInt(localStorage.getItem("score"))
+
+   score += points
+
+   // If score is a negative number, reset it to 0
+   if (score < 0) score = 0
+
+   localStorage.setItem("score", score)
+
+   scoreElement.innerText = localStorage.getItem("score")
+
+}
+
+function updateStarCount() {
+
+   let stars = parseInt(localStorage.getItem("stars"))
+
+   stars++
+
+   localStorage.setItem("stars", stars)
+
+   starsElement.innerText = localStorage.getItem("stars")
+
+}
+
+function updateLives(points) {
+
+   let lives = parseInt(localStorage.getItem("lives"))
+
+   lives += points
+
+   if (lives === 0) {
+      lostGame = true
+      return;
+   }
+
+   localStorage.setItem("lives", lives)
+
+   livesElement.innerText = localStorage.getItem("lives")
+
+}
+
+// Create player
+const player = new Player()
+
+// Missiles arrays
+let PLAYER_MISSILES = [new Missile()]
+
+let ENEMY_MISSILES = []
+
+// Enemies array
+let ENEMIES = [new Enemy()]
+
+// Stars array
+let STARS = []
+
+// Hearts array
+let HEARTS = []
+
+// Render initial position of player
+player.render()
+
+// Keep track of pressed keys for shooting and movement
+const pressedKeys = {
+   UP: false,
+   DOWN: false,
+   LEFT: false,
+   RIGHT: false,
+   SPACE: false
+}
+
+// Add new enemy every 1.5 seconds
+setInterval(() => {
+
+   if (runGame) ENEMIES.push(new Enemy())
+
+}, 1500)
+
+// Add new enemy star 3.5 seconds
+setInterval(() => {
+
+   if (runGame) STARS.push(new Star())
+
+}, 3500)
+
+// Add new heart every 4 seconds
+setInterval(() => {
+
+   if (runGame) HEARTS.push(new Heart())
+
+}, 4000)
+
+// End game
+function finishGame(animationFrame) {
+
+   clearInterval(timerInterval)
+
+   clearScreen = true
+
+   let scoreRecord = parseInt(localStorage.getItem("score-record"))
+   let starsRecord = parseInt(localStorage.getItem("stars-record"))
+
+   /* Update records if player achieved higher scores */
+   if (scoreRecord < localStorage.getItem("score")) localStorage.setItem("score-record", localStorage.getItem("score"))
+   if (starsRecord < localStorage.getItem("stars")) localStorage.setItem("stars-record", localStorage.getItem("stars"))
+
+   localStorage.setItem("lives", 3)
+   localStorage.setItem("stars", 0)
+   localStorage.setItem("score", 0)
+
+   ctx.clearRect(0, 0, canvas.width, canvas.height)
+
+   runGame = false
+
+   livesElement.innerText = "0"
+   starsElement.innerText = "0"
+   scoreElement.innerText = "0"
+
+   lostMessageElement.style.display = "block"
+}
+
+// Game loop
+function gameLoop() {
+   let animationFrame = requestAnimationFrame(gameLoop)
+
+   if (runGame) {
+      if (lostGame) finishGame(animationFrame)
+
+      // Clear canvas on each iteration
+      ctx.clearRect(0, 0, canvas.width, canvas.height)
+
+      if (pressedKeys.UP) player.moveInDirection("up")
+      if (pressedKeys.DOWN) player.moveInDirection("down")
+      if (pressedKeys.LEFT) player.moveInDirection("left")
+      if (pressedKeys.RIGHT) player.moveInDirection("right")
+
+      // If spacebar was pressed, shoot last missile
+      if (pressedKeys.SPACE) player.shoot(PLAYER_MISSILES[PLAYER_MISSILES.length - 1])
+
+      // Draw player on each iteration
+      player.render()
+
+      STARS.forEach((star, starIndex) => {
+
+         star.render()
+
+         if (star.collidedWith(player)) {
+
+            updateStarCount()
+
+            STARS = STARS.filter((el, ind) => ind !== starIndex)
+
+         }
+
+      })
+
+      HEARTS.forEach((heart, heartIndex) => {
+
+         heart.render()
+
+         if (heart.collidedWith(player)) {
+
+            updateLives(1)
+
+            HEARTS = HEARTS.filter((el, ind) => ind !== heartIndex)
+
+         }
+
+      })
+
+      ENEMIES.forEach((enemy, enemyIndex) => {
+
+         // If player missile collided with enemy, remove enemy
+         PLAYER_MISSILES.forEach((missile, missileIndex) => {
+
+            if (enemy.collidedWith(missile) && enemy.position.y >= 0) {
+
+               // Since enemy missiles live within the enemy instance, as soon as enemy is killed copy its missiles into another array
+
+               // Given that missiles of killed enemies will be drawn with the render method, set their velocity right 
+               ENEMY_MISSILES = [...ENEMY_MISSILES, ...enemy.MISSILES.map(missile => {
+                  missile.velocity = enemy.velocity
+                  return missile
+               })]
+
+               // Killed property to trigger blast
+               enemy.killed = true
+
+               // Remove enemy
+               ENEMIES = ENEMIES.filter((el, ind) => ind !== enemyIndex)
+
+               // Remove missile that killed enemy
+               PLAYER_MISSILES = PLAYER_MISSILES.filter((el, ind) => ind !== missileIndex)
+
+               updateScore(10)
+
+            }
+
+         })
+
+         // Draw enemies on each iteration
+         if (enemy.collidedWith(player)) lostGame = true
+
+         // Draw enemies on each iteration
+         enemy.render()
+
+         if (enemy.loaded) {
+
+            if (enemy.position.y >= canvas.height) {
+
+               // Delete enemy as soon as it is out of frame
+               ENEMIES = ENEMIES.filter((el, ind) => ind !== enemyIndex)
+
+               // Subtract 5 points from score if enemy was not killed
+               updateScore(-5)
+            }
+
+         }
+
+         // Shoot missiles every time it is rendered
+         enemy.shoot(player)
+      })
+
+      // Draw missiles on each iteration
+      PLAYER_MISSILES.forEach((missile, missileIndex) => {
+         missile.render()
+
+         if (missile.loaded) {
+            // Delete missile as soon as it is out of frame
+            if (missile.position.y <= -missile.image.height && missile.position.x >= 0) PLAYER_MISSILES = PLAYER_MISSILES.filter((el, ind) => ind !== missileIndex)
+         }
+      })
+
+      // If array of enemy missiles has elements, draw them
+      if (ENEMY_MISSILES.length) {
+         ENEMY_MISSILES.forEach((missile, missileIndex) => {
+            missile.render()
+
+            if (missile.loaded) {
+               // Delete missile as soon as it is out of frame
+               if (missile.position.y >= canvas.height) ENEMY_MISSILES = ENEMY_MISSILES.filter((el, ind) => ind !== missileIndex)
+            }
+
+            // Given that collision detection lived within enemy instances, it has to be detected again
+            if (player.collidedWith(missile)) {
+               updateLives(-1)
+               ENEMY_MISSILES = ENEMY_MISSILES.filter((el, ind) => ind !== missileIndex)
+            }
+         })
+      }
+
+      if (clearScreen) ctx.clearRect(0, 0, canvas.width, canvas.height)
+   }
+
 }
